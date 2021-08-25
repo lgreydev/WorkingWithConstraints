e156d2f0e1d0a32b93c7df36adf3a46bfcf74bf2
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-08-25 12:31:36 +0300

    Add stack views Flowers screen 1

--- WorkingWithConstraints/ViewController.swift
+++ WorkingWithConstraints/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
+
+
+extension UILabel  {
+    func radius() {
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 10
+    }
+}
+
+
+
 
