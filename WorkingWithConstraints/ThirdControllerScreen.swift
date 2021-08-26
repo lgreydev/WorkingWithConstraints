73b091a1285c6b633ebd88a06a8e076afa565a91
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-08-26 12:01:31 +0300

    Removed scroll view and added a new content to screen 3

---
+++ WorkingWithConstraints/ThirdControllerScreen.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdControllerScreen.swift
+//  WorkingWithConstraints
+//
+//  Created by Sergey Lukaschuk on 26.08.2021.
+//
+
+import UIKit
+
+class ThirdControllerScreen: UIViewController {
+    
+    // MARK: - Properties
+ 
+    
+    @IBOutlet var myView: UIView!
+    
+    
+    // MARK: - UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup
+        myView.layer.cornerRadius = 10
+    }
+    
+    // MARK: - Interface Builder actions
+    
+    
+}
